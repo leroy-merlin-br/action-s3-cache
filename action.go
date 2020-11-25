c9283c64eb25bb41e4a2c992ca0e3c988447178e
amendoa <almeida-matheus@hotmail.com.br>
2020-11-25 16:54:27 -0300

    feat: add action select

--- action.go
+++ action.go
@@ -1,19 +1,18 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func (action Action) Exec() error {
-	fmt.Print("go action :) \n")
-
-	fmt.Printf("action: %s \n", action.Action)
-	fmt.Printf("aws-secret-access-key: %s \n", action.AwsSecretAccessKey)
-	fmt.Printf("aws-access-key-id: %s \n", action.AwsAccessKeyId)
-	fmt.Printf("bucket: %s \n", action.Bucket)
-	fmt.Printf("key: %s \n", action.Key)
-	fmt.Printf("artifacts: %s \n", action.Artifacts)
-	fmt.Printf("artifacts len: %d \n", len(action.Artifacts))
+	switch act := action.Action; act {
+	case PutAction:
+		fmt.Print("PutAction")
+	case GetAction:
+		fmt.Print("GetAction")
+	case DeleteAction:
+		fmt.Print("DeleteAction")
+	default: 
+		fmt.Printf("Action \"%s\" is not allowed. Valid options are: [%s, %s, %s]", act, PutAction, DeleteAction, GetAction)
+	}
 
 	return nil
 }
