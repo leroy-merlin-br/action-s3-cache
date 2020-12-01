456d866898c8be220b5564551045e1d54a2710ba
amendoa <almeida-matheus@hotmail.com.br>
2020-12-01 13:48:23 -0300

    ops: set right working directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Select right go binary for runner os
-$ACTION_PATH/dist/$(echo "$OS" | tr "[:upper:]" "[:lower:]")
+$GITHUB_ACTION_PATH/dist/$(echo "$OS" | tr "[:upper:]" "[:lower:]")
